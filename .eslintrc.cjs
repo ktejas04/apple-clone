6d5e9383da4a2946213655a644aa12ed6b79afab
Tejas Kumar <tejas22052004@gmail.com>
2024-05-04 19:42:25 +0530

    Final Project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,10 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','@react-three'],
   rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import Chips from "./components/Chips";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
 
+import * as Sentry from "@sentry/react"
 
 const App = () => {
   return (
-    <div className="bg-black min-h-screen w-full">
-      <h1>Hello</h1>
-    </div>
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+      <Model/>
+      <Features/>
+      <Chips/>
+      <Footer/>
+
+    </main>
   );
+
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
 };
 
-export default App;
+export default Sentry.withProfiler(App);
